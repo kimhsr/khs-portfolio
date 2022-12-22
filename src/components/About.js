1cd55a9aec9821d11db35cf47a7ea31bca1b03f0
kimhsr <kimhsr@naver.com>
2022-12-22 21:55:19 +0900

    react-scroll 이용한 더 알아보기 버튼 구현

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./About.css";
 
 function About() {
-  return <div className="aboutBackground">About</div>;
+  return (
+    <div id="aboutScroll">
+      <div className="aboutBackground">
+        About
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-scroll";
 // import bannerImg from "../image/background_img.jpg";
 import "./Banner.css";
 
@@ -8,10 +9,9 @@ function Banner() {
       <div className="bannerContainer">
         <div className="bannerText">
           - 김현성 -
-          <br />
-          웹 개발자 포트폴리오
+          <br />웹 개발자 포트폴리오
         </div>
-        <hr className="bannerHr"/>
+        <hr className="bannerHr" />
         <div className="bannerTextBottom">
           안녕하세요.
           <br />
@@ -19,11 +19,12 @@ function Banner() {
           <br />
           나 혼자의 역량만이 아니라 내 주변,
           <br />
-          그리고 함께하는 팀의 성장을 도울 수 있는 개발자가 되기 위해 노력하고 있습니다.
+          그리고 함께하는 팀의 성장을 도울 수 있는 개발자가 되기 위해 노력하고
+          있습니다.
         </div>
-        <div className="bannerButton">
-          더 알아보기 ↓
-        </div>
+        <Link to="aboutScroll" spy={true} smooth={true} offset={-64}>
+          <div className="bannerButton">더 알아보기 ↓</div>
+        </Link>
       </div>
     </div>
   );
