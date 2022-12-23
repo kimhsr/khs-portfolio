b789fc03dcf625f90f0bd8db66043ad88787fe0d
kimhsr <kimhsr@naver.com>
2022-12-24 01:59:16 +0900

    글자 기울기 및 skills 제목 스타일링

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,7 +4,11 @@ import "./Skills.css";
 function Skills() {
   return (
     <div id="skillsScroll">
-      <div className="skillsBackground">Skills</div>
+      <div className="skillsBackground">
+        <div className="skillsContainer">
+          <div className="skillsText">SKILLS</div>
+        </div>
+      </div>
     </div>
   );
 }
