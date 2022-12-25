1b0554b16b7567b27b3489c64f4d026ce2ad0708
kimhsr <kimhsr@naver.com>
2022-12-26 01:05:19 +0900

    skills content 생성

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,8 +5,30 @@ function Skills() {
   return (
     <div id="skillsScroll">
       <div className="skillsBackground">
-        <div className="skillsContainer">
+        <div className="skillsTitle">
           <div className="skillsText">SKILLS</div>
+        </div>
+        <div className="skillsContainer">
+          <div className="skillsback">
+            <div className="skillsContent">Frontend</div>
+            <img alt=""></img>
+          </div>
+          <div className="skillsback">
+            <div className="skillsContent">Deployment</div>
+            <img alt=""></img>
+          </div>
+          <div className="skillsback">
+            <div className="skillsContent">Version Control</div>
+            <img alt=""></img>
+          </div>
+          <div className="skillsback">
+            <div className="skillsContent">Communication</div>
+            <img alt=""></img>
+          </div>
+          <div className="skillsback">
+            <div className="skillsContent">Certificate</div>
+            <img alt=""></img>
+          </div>
         </div>
       </div>
     </div>
