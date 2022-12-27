717c7ad868cf9c59bacb50e9c40636ecd7ba6acd
kimhsr <kimhsr@naver.com>
2022-12-27 22:22:00 +0900

    skills 수정 및 반응형 구현

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,21 +11,21 @@ function Skills() {
         <div className="skillsContainer">
           <div className="skillsback">
             <div className="skillsContent">Frontend</div>
-            <img alt="" />
+            <img alt="front" src="img/front.png" />
           </div>
           <div className="skillsback">
             <div className="skillsContent">Deployment</div>
-            <img alt="" />
+            <img alt="deployment" src="img/deployment.png" />
           </div>
           <div className="skillsback">
             <div className="skillsContent">Version Control</div>
-            <img alt="" />
+            <img alt="version" src="img/version.png" />
           </div>
-          <div className="skillsback">
+          {/* <div className="skillsback">
             <div className="skillsContent">Communication</div>
             <img alt="" />
           </div>
-          {/* <div className="skillsback">
+          <div className="skillsback">
             <div className="skillsContent">Certificate</div>
             <img alt="" />
           </div> */}
