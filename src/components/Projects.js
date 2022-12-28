3830cc0269f1da4b240e2396268fab12d306017b
kimhsr <kimhsr@naver.com>
2022-12-28 16:23:10 +0900

    projects 구현

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,12 +15,59 @@ function Projects() {
           </div>
           <div className="content">
             <img
-              alt="netflixClone"
-              src="img/netflixClone.png"
+              alt="netflix"
+              src="img/netflix.png"
               className="projectsImgNet"
             />
-            <div className="">
-              <b>넷플릭스 클론 웹사이트입니다.</b>
+            <div className="contentMainBack">
+              <div className="contentMain">
+                <b>포트폴리오용으로 제작된 NETFLIX 클론 코딩 프로젝트</b>
+                입니다.
+                <br />
+                React를 사용하였으며, 현재는 영화들의 간단한 정보만을
+                로딩합니다.
+                <br />
+                API는 themoviedb.org 의 api를 이용하였으며,
+                <br />
+                모든 영화 및 드라마 정보가 넷플릭스의 정보와 일치하지는
+                않습니다.
+                <br />
+                Swiper 모듈을 이용해서 슬라이드를 만들었습니다.
+                <br />
+                <br />
+                <a
+                  href="https://github.com/kimhsr/react-netflix-clone"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="contentButton">자세히 보기</div>
+                </a>
+              </div>
+              <div className="projectsDescription">
+                <div className="projectsDeTitle">✔ 주요 기능</div>
+                <div className="projectsDeContent">
+                  영화 재생, 영화 검색, 영화 상세정보
+                </div>
+              </div>
+              <div className="projectsDescription">
+                <div className="projectsDeTitle">✔ URL</div>
+                <a
+                  href="https://kimhsr.github.io/react-netflix-clone/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="projectsDeContent">
+                    kimhsr.github.io/react-netflix-clone
+                  </div>
+                </a>
+              </div>
+              <div className="projectsDescription">
+                <div className="projectsDeTitle">✔ SKILLS</div>
+                <div className="projectsDeContent">
+                  React.js, Styled Component, <br />
+                  The Movie DB API, react-router, swiper
+                </div>
+              </div>
             </div>
           </div>
         </div>
