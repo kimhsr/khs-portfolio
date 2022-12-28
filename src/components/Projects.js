6855c096f3d043649de4c13ac9863f2134518a60
kimhsr <kimhsr@naver.com>
2022-12-28 20:28:59 +0900

    skills css 수정 및 projects 반응형 테스트

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,67 +5,69 @@ function Projects() {
   return (
     <div id="projectsScroll">
       <div className="projectsBackground">
-        <div className="projectsTitle">
-          <div className="projectsText">PROJECTS</div>
-        </div>
-        <div className="projectsContainer">
-          <div className="projectsContent">넷플릭스 클론 웹사이트</div>
-          <div className="projectsDate">
-            2022.11 ~ 2022.12 (1人 개인 프로젝트)
+        <div className="tlqkf">
+          <div className="projectsTitle">
+            <div className="projectsText">PROJECTS</div>
           </div>
-          <div className="content">
-            <img
-              alt="netflix"
-              src="img/netflix.png"
-              className="projectsImgNet"
-            />
-            <div className="contentMainBack">
-              <div className="contentMain">
-                <b>포트폴리오용으로 제작된 NETFLIX 클론 코딩 프로젝트</b>
-                입니다.
-                <br />
-                React를 사용하였으며, 현재는 영화들의 간단한 정보만을
-                로딩합니다.
-                <br />
-                API는 themoviedb.org 의 api를 이용하였으며,
-                <br />
-                모든 영화 및 드라마 정보가 넷플릭스의 정보와 일치하지는
-                않습니다.
-                <br />
-                Swiper 모듈을 이용해서 슬라이드를 만들었습니다.
-                <br />
-                <br />
-                <a
-                  href="https://github.com/kimhsr/react-netflix-clone"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <div className="contentButton">자세히 보기</div>
-                </a>
-              </div>
-              <div className="projectsDescription">
-                <div className="projectsDeTitle">✔ 주요 기능</div>
-                <div className="projectsDeContent">
-                  영화 재생, 영화 검색, 영화 상세정보
+          <div className="projectsContainer">
+            <div className="projectsContent">넷플릭스 클론 웹사이트</div>
+            <div className="projectsDate">
+              2022.11 ~ 2022.12 (1人 개인 프로젝트)
+            </div>
+            <div className="content">
+              <img
+                alt="netflix"
+                src="img/netflix.png"
+                className="projectsImgNet"
+              />
+              <div className="contentMainBack">
+                <div className="contentMain">
+                  <b>포트폴리오용으로 제작된 NETFLIX 클론 코딩 프로젝트</b>
+                  입니다.
+                  <br />
+                  React를 사용하였으며, 현재는 영화들의 간단한 정보만을
+                  로딩합니다.
+                  <br />
+                  API는 themoviedb.org 의 api를 이용하였으며,
+                  <br />
+                  모든 영화 및 드라마 정보가 넷플릭스의 정보와 일치하지는
+                  않습니다.
+                  <br />
+                  Swiper 모듈을 이용해서 슬라이드를 만들었습니다.
+                  <br />
+                  <br />
+                  <a
+                    href="https://github.com/kimhsr/react-netflix-clone"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <div className="contentButton">자세히 보기</div>
+                  </a>
                 </div>
-              </div>
-              <div className="projectsDescription">
-                <div className="projectsDeTitle">✔ URL</div>
-                <a
-                  href="https://kimhsr.github.io/react-netflix-clone/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
+                <div className="projectsDescription">
+                  <div className="projectsDeTitle">✔ 주요 기능</div>
                   <div className="projectsDeContent">
-                    kimhsr.github.io/react-netflix-clone
+                    영화 재생, 영화 검색, 영화 상세정보
                   </div>
-                </a>
-              </div>
-              <div className="projectsDescription">
-                <div className="projectsDeTitle">✔ SKILLS</div>
-                <div className="projectsDeContent">
-                  React.js, Styled Component, <br />
-                  The Movie DB API, react-router, swiper
+                </div>
+                <div className="projectsDescription">
+                  <div className="projectsDeTitle">✔ URL</div>
+                  <a
+                    href="https://kimhsr.github.io/react-netflix-clone/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <div className="projectsDeContent">
+                      kimhsr.github.io/react-netflix-clone
+                    </div>
+                  </a>
+                </div>
+                <div className="projectsDescription">
+                  <div className="projectsDeTitle">✔ SKILLS</div>
+                  <div className="projectsDeContent">
+                    React.js, Styled Component, <br />
+                    The Movie DB API, react-router, swiper
+                  </div>
                 </div>
               </div>
             </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,23 +5,24 @@ function Skills() {
   return (
     <div id="skillsScroll">
       <div className="skillsBackground">
-        <div className="skillsTitle">
-          <div className="skillsText">SKILLS</div>
-        </div>
-        <div className="skillsContainer">
-          <div className="skillsback">
-            <div className="skillsContent">Frontend</div>
-            <img alt="front" src="img/front.png" />
+        <div className="skillsPadding">
+          <div className="skillsTitle">
+            <div className="skillsText">SKILLS</div>
           </div>
-          <div className="skillsback">
-            <div className="skillsContent">Deployment</div>
-            <img alt="deployment" src="img/deployment.png" />
-          </div>
-          <div className="skillsback">
-            <div className="skillsContent">Version Control</div>
-            <img alt="version" src="img/version.png" />
-          </div>
-          {/* <div className="skillsback">
+          <div className="skillsContainer">
+            <div className="skillsback">
+              <div className="skillsContent">Frontend</div>
+              <img alt="front" src="img/front.png" />
+            </div>
+            <div className="skillsback">
+              <div className="skillsContent">Deployment</div>
+              <img alt="deployment" src="img/deployment.png" />
+            </div>
+            <div className="skillsback">
+              <div className="skillsContent">Version Control</div>
+              <img alt="version" src="img/version.png" />
+            </div>
+            {/* <div className="skillsback">
             <div className="skillsContent">Communication</div>
             <img alt="" />
           </div>
@@ -29,6 +30,7 @@ function Skills() {
             <div className="skillsContent">Certificate</div>
             <img alt="" />
           </div> */}
+          </div>
         </div>
       </div>
     </div>
