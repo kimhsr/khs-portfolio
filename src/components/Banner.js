ec40eaca912faba53c5c6e17878228c0d17b77ac
kimhsr <kimhsr@naver.com>
2022-12-28 22:01:39 +0900

    banner height 수정

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,25 +6,27 @@ import "./Banner.css";
 function Banner() {
   return (
     <div className="bannerBackground">
-      <div className="bannerContainer">
-        <div className="bannerText">
-          - 김현성 -
-          <br />웹 개발자 포트폴리오
+      <div className="bannerPadding">
+        <div className="bannerContainer">
+          <div className="bannerText">
+            - 김현성 -
+            <br />웹 개발자 포트폴리오
+          </div>
+          <hr className="bannerHr" />
+          <div className="bannerTextBottom">
+            안녕하세요.
+            <br />
+            새로운 지식과 기술을 배우는 것을 좋아하는 프론트엔드 개발자입니다.
+            <br />
+            나 혼자의 역량만이 아니라 내 주변,
+            <br />
+            그리고 함께하는 팀의 성장을 도울 수 있는 개발자가 되기 위해 노력하고
+            있습니다.
+          </div>
+          <Link to="aboutScroll" spy={true} smooth={true} offset={-75}>
+            <div className="bannerButton">더 알아보기 ↓</div>
+          </Link>
         </div>
-        <hr className="bannerHr" />
-        <div className="bannerTextBottom">
-          안녕하세요.
-          <br />
-          새로운 지식과 기술을 배우는 것을 좋아하는 프론트엔드 개발자입니다.
-          <br />
-          나 혼자의 역량만이 아니라 내 주변,
-          <br />
-          그리고 함께하는 팀의 성장을 도울 수 있는 개발자가 되기 위해 노력하고
-          있습니다.
-        </div>
-        <Link to="aboutScroll" spy={true} smooth={true} offset={-75}>
-          <div className="bannerButton">더 알아보기 ↓</div>
-        </Link>
       </div>
     </div>
   );
