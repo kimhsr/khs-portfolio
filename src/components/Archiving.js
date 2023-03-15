055b8fb5fbfd42ccea10bed074e7883f5169a19b
kimhsr <kimhsr@naver.com>
2023-03-15 21:32:20 +0900

    포트폴리오 수정

--- src/components/Archiving.js
+++ src/components/Archiving.js
@@ -25,13 +25,37 @@ function Archiving() {
               </a>
               <div className="archivingDescription">
                 <p>
-                  <b>소스 코드 저장소</b>
-                  입니다.
+                  <b>소스 코드 저장소</b>입니다.
                 </p>
                 <ul>
                   <li>과거 프로젝트, 프로그램 소스 코드</li>
                   <li>혼자서 코딩 연습을 위해 끄적이던 소스 코드</li>
                   <li>교육 수강한 수업들의 코딩 과제 소스 코드</li>
+                </ul>
+              </div>
+            </div>
+            <div className="archivingback" id="archivingback2">
+              <img
+                alt="velogImg"
+                src={`${process.env.PUBLIC_URL}/img/Velog.png`}
+                className="archivingImg2"
+              />
+              <a
+                href="https://velog.io/@kimhsr"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="archivingUrl">velog.io/@kimhsr</div>
+              </a>
+              <div className="archivingDescription">
+                <p>
+                  <b>복습하려고 만든 블로그</b>
+                  입니다.
+                </p>
+                <ul>
+                  <li>배운 내용 복슴 겸 정리</li>
+                  <li>자주 검색하게 되는 내용 기록</li>
+                  <li>까먹어도 쉽게 찾을 수 있는 에러 기록</li>
                 </ul>
               </div>
             </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,6 +11,77 @@ function Projects() {
           </div>
           <div className="projectsContainer">
             <div className="projectsContent">포트폴리오 웹사이트</div>
+            <div className="projectsDate">
+              2023.02 ~ 2023.03 (1人 개인 프로젝트)
+            </div>
+            <div className="content">
+              <img
+                alt="portfolio"
+                src={`${process.env.PUBLIC_URL}/img/redditProject.png`}
+                className="projectsImgNet"
+              />
+              <div className="contentMainBack">
+                <div className="contentMain">
+                  <b>Next.js의 학습을 위해 reddit을 클론 코딩 한 웹사이트</b>
+                  입니다.
+                  <br />
+                  프론트엔드 부분은 리액트의 NextJs를 사용하고,
+                  <br />
+                  백엔드 부분은 NodeJs와 ExpressJs를 사용했습니다.
+                  <br />
+                  두 부분 모두 언어는 타입스크립트를 사용했습니다.
+                  <br />
+                  데이터베이스는 PostgreSQL을 이용하며 TypeORM을 이용해서 컨트롤
+                  했습니다.
+                  <br />
+                  Next.js의 학습 방법과
+                  <br />
+                  백엔드와 데이터베이스가 어떻게 돌아가는지 알 수 있는 좋은
+                  기회였습니다.
+                  <br />
+                  <br />
+                  <a
+                    href="https://github.com/kimhsr/reddit-project"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <div className="contentButton">자세히 보기</div>
+                  </a>
+                </div>
+                <div className="projectsDescription">
+                  <div className="projectsDeTitle">✔ 주요 기능</div>
+                  <div className="projectsDeContent">
+                    로그인, 회원가입, 커뮤니티 생성, 이미지 업로드
+                    <br />
+                    게시글 작성, 댓글, 투표 기능
+                  </div>
+                </div>
+                <div className="projectsDescription">
+                  <div className="projectsDeTitle">✔ URL</div>
+                  <a
+                    href="http://reddit-project.kro.kr/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <div className="projectsDeContent">
+                      reddit-project.kro.kr
+                    </div>
+                  </a>
+                </div>
+                <div className="projectsDescription">
+                  <div className="projectsDeTitle">✔ SKILLS</div>
+                  <div className="projectsDeContent">
+                    Next.js, Typescript, Tailwind CSS,
+                    <br />
+                    ExpressJS, Postgres, Doker
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="projectsContainer">
+            <div className="projectsContent">포트폴리오 웹사이트</div>
             <div className="projectsDate">2022.12 (1人 개인 프로젝트)</div>
             <div className="content">
               <img
